bb59fda7a2acb9a13af065a53f13b6a3aec798cd
ismail <ismail.x@aol.com>
2020-09-11 19:48:32 +0700

    Initial Commit

---
+++ GambarCuaca/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GambarCuaca
+//
+//  Created by Ismail . on 11/09/20.
+//  Copyright © 2020 Ismail . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
